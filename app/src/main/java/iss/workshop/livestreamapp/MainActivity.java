41dc608584872a843d8c1796e9a77251492f964d
Gab Reyes <gc31.reyes@gmail.com>
2022-07-30 14:44:43 +0800

    first commit of livestream sample

---
+++ app/src/main/java/iss/workshop/livestreamapp/MainActivity.java
@@ -0,0 +1,121 @@
+package iss.workshop.livestreamapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+import android.os.Bundle;
+
+import io.agora.rtc2.Constants;
+import io.agora.rtc2.IRtcEngineEventHandler;
+import io.agora.rtc2.RtcEngine;
+import io.agora.rtc2.RtcEngineConfig;
+import io.agora.rtc2.video.VideoCanvas;
+import io.agora.rtc2.ChannelMediaOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Fill the App ID of your project generated on Agora Console.
+    private String appId = "813f22ea50924b43ae8488edb975d02c";
+    // Fill the channel name.
+    private String channelName = "Test Channel";
+    // Fill the temp token generated on Agora Console.
+    private String token = "006813f22ea50924b43ae8488edb975d02cIADRJOVjXUHELNJdyGwxlpOArNOtenRN0TsDkMeQS/4mJtc7RNwAAAAAEACGukDPFxzmYgEAAQAWHOZi";
+
+    private RtcEngine mRtcEngine;
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        @Override
+        // Listen for the remote host joining the channel to get the uid of the host.
+        public void onUserJoined(int uid, int elapsed) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    // Call setupRemoteVideo to set the remote video view after getting uid from the onUserJoined callback.
+                    setupRemoteVideo(uid);
+                }
+            });
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(REQUESTED_PERMISSIONS[0], PERMISSION_REQ_ID) &&
+                checkSelfPermission(REQUESTED_PERMISSIONS[1], PERMISSION_REQ_ID)) {
+            initializeAndJoinChannel();
+        }
+    }
+
+    private static final int PERMISSION_REQ_ID = 22;
+    private static final String[] REQUESTED_PERMISSIONS = {
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.CAMERA
+    };
+
+    private boolean checkSelfPermission(String permission, int requestCode) {
+        if (ContextCompat.checkSelfPermission(this, permission) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, REQUESTED_PERMISSIONS, requestCode);
+            return false;
+        }
+        return true;
+    }
+
+    private void initializeAndJoinChannel() {
+        try {
+            RtcEngineConfig config = new RtcEngineConfig();
+            config.mContext = getBaseContext();
+            config.mAppId = appId;
+            config.mEventHandler = mRtcEventHandler;
+            mRtcEngine = RtcEngine.create(config);
+        } catch (Exception e) {
+            throw new RuntimeException("Check the error.");
+        }
+        // By default, video is disabled, and you need to call enableVideo to start a video stream.
+        mRtcEngine.enableVideo();
+        // Start local preview.
+        mRtcEngine.startPreview();
+
+        FrameLayout container = findViewById(R.id.local_video_view_container);
+        // Create a SurfaceView object and add it as a child to the FrameLayout.
+        SurfaceView surfaceView = new SurfaceView (getBaseContext());
+        container.addView(surfaceView);
+        // Pass the SurfaceView object to Agora so that it renders the local video.
+        mRtcEngine.setupLocalVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, 0));
+
+        ChannelMediaOptions options = new ChannelMediaOptions();
+        // Set the client role as BROADCASTER or AUDIENCE according to the scenario.
+        options.clientRoleType = Constants.CLIENT_ROLE_BROADCASTER;
+        // For the Interactive Live Streaming Standard scenario, the user level of an audience member needs to be AUDIENCE_LATENCY_LEVEL_LOW_LATENCY.
+        options.audienceLatencyLevel = Constants.AUDIENCE_LATENCY_LEVEL_LOW_LATENCY;
+        // For a live streaming scenario, set the channel profile as BROADCASTING.
+        options.channelProfile = Constants.CHANNEL_PROFILE_LIVE_BROADCASTING;
+
+        // Join the channel with a temp token.
+        // You need to specify the user ID yourself, and ensure that it is unique in the channel.
+        mRtcEngine.joinChannel(token, channelName, 0, options);
+    }
+
+    private void setupRemoteVideo(int uid) {
+        FrameLayout container = findViewById(R.id.remote_video_view_container);
+        SurfaceView surfaceView = new SurfaceView (getBaseContext());
+        surfaceView.setZOrderMediaOverlay(true);
+        container.addView(surfaceView);
+        mRtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_FIT, uid));
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRtcEngine.stopPreview();
+        mRtcEngine.leaveChannel();
+    }
+
+}
